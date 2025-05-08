b9dfe8b92936b8ddb0cd323261979878fc14da22
THEKINGSTAR <thekingstar5002@gmail.com>
2025-05-09 02:21:54 +0300

    add the index.js

--- api/courses/index.js
+++ api/courses/index.js
@@ -1,6 +1,10 @@
 const connectDB = require('../../utils/db');
 const Course = require('../../models/Course');
 
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'Course Management API is running.' });
+};
+
 module.exports = async (req, res) => {
   await connectDB();
 
@@ -21,3 +25,4 @@ module.exports = async (req, res) => {
 
   return res.status(405).json({ error: 'Method not allowed' });
 };
+
---
+++ api/index.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'Course Management API is running.' });
+};
